ab5ddb54d411c7413039bfc68ad24306d64f7fc2
KaiTand <kai.thomas.anderson@gmail.com>
2014-11-30 21:10:33 -0700

    Just the data structures

--- kai.go
+++ kai.go
@@ -1,1 +1,53 @@
 package main
+
+type Pair struct {
+	Key   string
+	Value string
+}
+
+type Config struct {
+	Master    string
+	InputData string
+	Output    string
+	M         int
+	R         int
+	Table     string
+}
+
+type Request struct {
+	Message string
+	Address string
+	Type    int
+	Work    Work
+}
+
+type Master struct {
+	MapAssi      int
+	FilesDL      int
+	RFilesDL     int
+	Address      string
+	M            int
+	R            int
+	Maps         []Work
+	MapAddresses []string
+	MapDoneCount int
+	ReduceCount  int
+	WorkDone     int
+	DoneChan     chan int
+	Merged       bool
+	Table        string
+	Output       string
+	Verb         bool
+}
+
+type Work struct {
+	WorkerID     int
+	Type         int
+	Filename     string
+	Offset       int
+	Size         int
+	M            int
+	R            int
+	Table        string
+	MapAddresses []string
+}
